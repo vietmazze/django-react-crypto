d6affce525f2e4375e3f43957598cd0b0889ff7d
tangtang <gherson7827123@gmail.com>
2019-02-06 21:38:25 -0500

    Add files via upload

---
+++ src/components/detailmarketcap.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import { Table, Divider, Tag } from 'antd';
+
+
+
+const { Column, ColumnGroup } = Table;
+
+
+
+class  Detailmarketcap extends React.Component  {
+    render() {
+      // const{text} = this.props;
+      // console.log("Is this text", text)
+    return (
+    <div>test</div>
+    
+    )
+    }
+}
+
+
+
+const mapStateToProps = state => {
+  return {
+    coindetail: state.coinformdetailReducer.data
+  }
+}
+
+
+export default Detailmarketcap;
---
+++ src/components/marketcap.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import { Modal, Button } from 'antd';
+import Detailmarketcap from '../components/detailmarketcap';
+
+import {
+    Table, Badge, Menu, Dropdown, Icon,
+  } from 'antd';
+  import * as actions from '../store/actions/fetch';
+ 
+  
+class  Marketcap extends React.Component  {
+  state = { visible: false,
+            text:{} }
+
+ 
+
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  } 
+  
+  handleOnclick =(text) => {
+    window.location.assign('http://localhost:3000/marketcap/'+ text);
+  }
+  
+  
+  render() {
+ 
+    // {
+    //   this.props.coindetail(this.props.token);
+    // }
+    const columns = [
+        { title: 'Symbol', dataIndex: 'symbol', key: 'symbol', 
+        render: text => <a  href="javascript:void(0)" onClick={() => this.handleOnclick(text)}>{text}</a>,},
+        { title: 'Marketcap', dataIndex: 'marketcap', key: 'marketcap' },
+        { title: 'Price',dataIndex:'price', key:'price' },
+        { title: 'Circulating Supply',dataIndex:'currentSupply', key:'currentSupply' },
+    ];
+  
+  
+    return (
+      <div>
+      <Table
+        className="components-table-demo-nested"
+        columns={columns}
+        dataSource={this.props.data} //replace with coinform filter by coin item
+        rowKey = {this.props.data.owner}
+      />
+      
+      </div>
+    );
+  }
+}
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     coindetail: (token) => dispatch(actions.fetchCoinformdetail(token)) 
+//   }
+// }
+
+const mapStateToProps = state => {
+    return {
+        porfolio: state.PorfolioReducer.data,
+        data: state.fetchReducer.data,
+        token: state.authReducer.token
+    }
+}
+
+export default connect(mapStateToProps) (Marketcap);
+  
